67c36c5dca0b769b2c0b904129c92215cf1c6154
liushifu <872140945@qq.com>
2020-04-04 09:31:03 +0800

    php实现的单链表数据结构的定义及操作 用php对象作为链表的节点，php对象是引用数据

--- dataScala.php
+++ dataScala.php
@@ -95,45 +95,46 @@ class arrayQueue
     }
 }
 
-
+//=================================================== 队列的操作
 //$queue = new ArrayQueue();
-$queue = new CircleArrayQueue();
+//$queue = new CircleArrayQueue();
 //$queue->isEmpty();
 //$queue->isFull();
-while (true) {
-    echo "show：打印队列" . PHP_EOL;
-    echo "add：添加数据" . PHP_EOL;
-    echo "out：取数据" . PHP_EOL;
-    echo "head：查看队首数据" . PHP_EOL;
-    echo "size：查看队列大小" . PHP_EOL;
-    echo "exit：退出" . PHP_EOL;
-    $line = trim(fgets(STDIN));
-    switch ($line) {
-        case "show":
-            $queue->showQueue();
-            break;
-        case "head":
-            $queue->headNode();
-            break;
-        case "size":
-            $queue->sizeQueue();
-            break;
-        case "add":
-            echo "  请输入入队数据:";
-            $line = trim(fgets(STDIN));
-            $queue->addQueue($line);
-            break;
-        case "out":
-            $queue->outQueue();
-            break;
-        case "exit":
-            break 2;
-        default:
-            echo "输入错误" . PHP_EOL;
-    }
+//while (true) {
+//    echo "show：打印队列" . PHP_EOL;
+//    echo "add：添加数据" . PHP_EOL;
+//    echo "out：取数据" . PHP_EOL;
+//    echo "head：查看队首数据" . PHP_EOL;
+//    echo "size：查看队列大小" . PHP_EOL;
+//    echo "exit：退出" . PHP_EOL;
+//    $line = trim(fgets(STDIN));
+//    switch ($line) {
+//        case "show":
+//            $queue->showQueue();
+//            break;
+//        case "head":
+//            $queue->headNode();
+//            break;
+//        case "size":
+//            $queue->sizeQueue();
+//            break;
+//        case "add":
+//            echo "  请输入入队数据:";
+//            $line = trim(fgets(STDIN));
+//            $queue->addQueue($line);
+//            break;
+//        case "out":
+//            $queue->outQueue();
+//            break;
+//        case "exit":
+//            break 2;
+//        default:
+//            echo "输入错误" . PHP_EOL;
+//    }
+//
+//}
 
-}
-
+/* ====================循环队列，环形队列==============================*/
 /**
  * 优化版
  * 数组队列基础上增加【取模】运算，使得队列逻辑上是一个环形队列
@@ -259,3 +260,185 @@ class CircleArrayQueue
         return ($this->r - $this->f + count($this->arr)) % count($this->arr);
     }
 }
+
+/* ========================单链表====================== */
+
+/**
+ * 组成链表的节点元素
+ * 数据类型：定义一种数据结构及围绕这一数据结构可以进行的操作（增删改查）
+ * Class HeroNode
+ */
+class HeroNode{
+    /**
+     * 业务字段，编号
+     * @var
+     */
+    public $no;
+
+    /**
+     * 业务字段，名称
+     * @var
+     */
+    public $name;
+
+    /**
+     * 必要的非业务字段：指针
+     * 指向下一个节点的指针
+     * 每个新的节点初始化时都是null
+     * @var
+     */
+    public $next = null;
+
+    public function __construct($no,$name)
+    {
+        $this->no = $no;
+        $this->name = $name;
+    }
+}
+
+/**
+ * 定义一个单链表及允许的操作
+ */
+class SingleLinkedList {
+
+    /**
+     * 头节点（头指针）
+     * 是一个固定的，没有实际业务意义的对象，专门指向链表的第一个节点
+     * @var
+     */
+    public $head;
+
+    /**
+     * 单链表初始化时，必有头节点
+     * SingleLinkedList constructor.
+     */
+    public function __construct()
+    {
+        $this->head = new HeroNode(0,"");
+    }
+
+    /**
+     * 添加节点
+     * 在列表的尾部，添加节点
+     * 其实就是先找到队尾的节点，把新节点链入最后节点即可；
+     *
+     * 注意：
+     *  头节点如果为空，则需要先创建头节点
+     *  但是为了减少每次的判断，
+     *  就默认头节点已经创建的情况下，才能调用该方法添加普通业务节点
+     * @param HeroNode $node
+     * @author: LiuShiFu
+     */
+    public function add(HeroNode $node) {
+        //从头节点开始
+        $temp = $this->head;
+        while(true) {
+            //当前节点$temp的下一个节点为空，则说明找到最后一个节点了
+            if ($temp->next == null) {
+                break;
+            }
+            $temp = $temp->next;
+        }
+        //php没有指针，但是每个节点都是用数据类型Object实现的，
+        //php中对象是按照引用传递的（形参，实参传递的是引用的值，仍然值传递），并不是真正内存中的对象数据
+        //在真正输出Object数据时，php发现是Object类型，底层C语言就知道存储的是指针，就会输出指针背后的真正的对象数据；
+        //头节点head也是对象
+        $temp->next = $node;
+    }
+
+
+    /**
+     * 删除链表的某个节点
+     * 当然是根据普通业务节点的业务，比如编号
+     *
+     * 注意：
+     *  单链表，只能根据当前节点，找下一个节点，不能反向寻找，所以一般只删除末尾节点，容易些，
+     * 删除中间节点，比较麻烦；
+     * 所以关键点是找到，被删除节点的上一个节点，那么$temp->next就是要被删除的节点
+     * @param HeroNode $node
+     * @author: LiuShiFu
+     */
+    public function del(HeroNode $node)
+    {
+        if ($this->isEmpty()) {
+            echo "链表为空：".PHP_EOL;
+            return;
+        }
+        $flag = false;
+        $temp = $this->head;
+        echo "删除节点：".PHP_EOL;
+        //遍历的条件是，下一个节点不为空
+        while($temp->next != null) {
+            //下一个节点的删除条件判断：编号
+            if ($temp->next->no == $node->no) {
+                $flag = true;
+                //恰好是末尾的节点
+                if ($temp->next->next == null) {
+                    printf("\t hno=%d,hname=%s".PHP_EOL,$temp->next->no,$temp->next->name);
+                    $temp->next = null;//删除，不要用unset
+                    break;
+                }
+                //不是末尾节点，需要注意
+                $next2 = $temp->next->next;//被删除节点的下一个节点
+                $temp->next = null;//删除节点；
+                $temp->next = $next2;//后续节点继续拼接上
+                break;
+            }
+            //没有找到，继续遍历
+            $temp = $temp->next;
+        }
+        //没有找到那个节点，或者成功删除了那个节点
+        if (!$flag) {
+            echo "没有找到该节点：".sprintf("\t hno=%d,hname=%s".PHP_EOL,$node->no,$node->name);
+        }
+    }
+
+    /**
+     * 链表为空的判断
+     * @return bool
+     * @author: LiuShiFu
+     */
+    public function isEmpty() {
+        //只有头节点的情况，链表为空
+        return $this->head->next == null;
+    }
+
+    /**
+     * 打印链表
+     * 遍历节点输出节点数据
+     * @author: LiuShiFu
+     */
+    public function show() {
+
+        if ($this->isEmpty()) {
+            echo "链表为空：".PHP_EOL;
+            return;
+        }
+
+        $temp = $this->head;
+        echo "打印链表：".PHP_EOL;
+        while($temp != null) {
+            printf("\t hno=%d,hname=%s".PHP_EOL,$temp->no,$temp->name);
+            $temp = $temp->next;
+        }
+    }
+}
+//=================================================== 链表的操作
+$list = new SingleLinkedList();
+$h1 = new HeroNode(1,"松江");
+$h2 = new HeroNode(2,"林冲");
+$h3 = new HeroNode(3,"玉麒麟");
+
+//添加节点
+$list->add($h1);
+$list->add($h2);
+$list->add($h3);
+
+//打印下节点
+$list->show();
+
+//删除中间节点
+$list->del($h2);
+$list->show();
+
+
